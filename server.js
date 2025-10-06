f77ab6c6f6181bc81f8492829026c582415f2ffc
chelsea Hwang <chelseahwang@chelseas-Air.lan>
2025-10-05 21:06:35 -0400

    converting ejs files

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
     const app = express();
     const port = 3000; // Or any desired port
+    const path = require('path'); // Required for path.join
+
 
     // Serve static files (HTML, CSS, images, JS) from a 'public' directory
     app.use(express.static('public'));
@@ -12,6 +14,45 @@ const express = require('express');
 
     // Start the server
     app.listen(port, () => {
-      console.log(`Server listening at http://localhost:${port}`);
+      console.log(`Server listening at localhost:${port}`);
     });
+    // Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Define the directory where your EJS files (views) are located
+// By default, Express looks for a 'views' directory in the root of your project
+app.set('views', path.join(__dirname, 'views'));
+app.get('/ejs1', (req, res) => {
+        res.render('ejsBlog1', { 
+            title: 'First snippet', 
+            message: 'From the blog' 
+        });
+    });
+app.get('/ejs2', (req, res) => {
+        res.render('ejsBlog2', { 
+            title: 'second snippet', 
+            message: 'From the blog' 
+        });
+    });
+app.get('/ejs5', (req, res) => {
+        res.render('ejsBlog5', { 
+            title: 'fifth snippet', 
+            message: 'From the blog' 
+        });
+    });
+app.get('/ejs6', (req, res) => {
+        res.render('ejsBlog6', { 
+            title: 'sixth snippet', 
+            message: 'From the blog' 
+        });
+    });
+app.get('/ejs7', (req, res) => {
+        res.render('ejsBlog7', { 
+            title: 'seventh snippet', 
+            message: 'From the blog' 
+        });
+    });
+
+
+
     
